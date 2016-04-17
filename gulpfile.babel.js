3db956b9643e5281cdb1e6e35bfc1b16419fef4b
Stuart Hammar <stuart.hammar@gmail.com>
2016-04-17 20:25:59 +0200

    Icons example

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,17 +2,39 @@ import gulp from 'gulp';
 import sass from 'gulp-sass';
 import autoprefixer from 'gulp-autoprefixer';
 import del from 'del';
+import svgSprite from 'gulp-svg-sprite';
+import cheerio from 'gulp-cheerio';
 
 const src = {
-  scss: './src/scss'
+  scss: './src/scss',
+  icons: './src/icons'
 };
 
 const dest = {
-  css: './dist/css'
+  css: './dist/css',
+  icons: './dist/icons'
 };
 
 const scssOpts = {
   outputStyle: 'compact'
+};
+
+const svgSpriteOpts = {
+  shape: {
+    dimension: {
+      maxWidth: 64,
+      maxHeight: 64
+    },
+    spacing: {
+      padding: 2
+    }
+  },
+  mode: {
+    symbol: true
+  },
+  svg: {
+    xmlDeclaration: false,
+  },
 };
 
 const autoprefixerOpts = {
@@ -20,7 +42,7 @@ const autoprefixerOpts = {
   cascade: false
 };
 
-gulp.task('scss', () =>
+gulp.task('scss', ['clean'], () =>
   gulp.src(`${src.scss}/**/base.scss`)
     .pipe(sass(scssOpts)
     .on('error', sass.logError))
@@ -28,6 +50,19 @@ gulp.task('scss', () =>
     .pipe(gulp.dest(dest.css))
 );
 
+gulp.task('svg', ['clean'], () =>
+  gulp.src(`${src.icons}/*.svg`)
+    .pipe(svgSprite(svgSpriteOpts))
+    .pipe(cheerio({
+      run ($) {
+        $('symbol:not([id$="-color"]) [fill]').removeAttr('fill');
+        $('[fill-rule]').removeAttr('fill-rule');
+      },
+      parserOptions: { xmlMode: true }
+    }))
+    .pipe(gulp.dest(dest.icons))
+);
+
 gulp.task('clean', () => del(['./dist']));
 
-gulp.task('default', ['clean', 'scss']);
+gulp.task('default', ['scss', 'svg']);
